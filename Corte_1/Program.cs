b25d68fb719497f7e081facc639b72b2f252d79c
Henry-COL <HenryQuinteroCO@gmail.com>
2026-02-27 11:59:53 -0500

    Update 0.0.031

--- Corte_1/Program.cs
+++ Corte_1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using Corte_1.Ejercicios_Basicos.App_Musica;
+using Corte_1.Ejercicios_Basicos.EJ_Tema1.Aforo;
+using Corte_1.Ejercicios_Basicos.EJ_Tema1.Generador_Correo;
+using Corte_1.Ejercicios_Basicos.EJ_Tema1.Pefil_Gamer;
+using Corte_1.Ejercicios_Basicos.EJ_Tema1.Propina;
+using Corte_1.Ejercicios_Basicos.EJ_Tema1.Simulador_Semaforo;
 using Corte_1.Ejercicios_Basicos.Inventario;
 using Corte_1.Ejercicios_Basicos.Sistema_Mascotas;
 using Corte_1.Ejercicios_Basicos.Sitema_Biblioteca;
@@ -20,16 +25,21 @@ namespace Corte_1
             while (continuar)
             {
                 Console.Clear();
-                Console.WriteLine("╔════════════════════════════════════════╗");
-                Console.WriteLine("║        SISTEMA MULTI-EJERCICIOS        ║");
-                Console.WriteLine("╠════════════════════════════════════════╣");
-                Console.WriteLine("║ 1. Mascota Virtual                     ║");
-                Console.WriteLine("║ 2. Inventario de Tienda                ║");
-                Console.WriteLine("║ 3. App de Música                       ║");
-                Console.WriteLine("║ 4. Sistema Biblioteca                  ║");
-                Console.WriteLine("║ 5. Vehículos Eléctricos                ║");
-                Console.WriteLine("║ 0. Salir                               ║");
-                Console.WriteLine("╚════════════════════════════════════════╝");
+                Console.WriteLine("===== SISTEMA DE EJERCICIOS BÁSICOS =====");
+                Console.WriteLine("TEMA 1:");
+                Console.WriteLine("1. Perfil Gamer.");
+                Console.WriteLine("2. Calculadora Propina Solidaria.");
+                Console.WriteLine("3. Control de Aforo.");
+                Console.WriteLine("4. Generador de Correo.");
+                Console.WriteLine("5. Simulador Semaforo");
+                Console.WriteLine("\nTEMA 2:");
+                Console.WriteLine("6. Mascota Virtual.");
+                Console.WriteLine("7. Inventario de Tienda.");
+                Console.WriteLine("8. Aplicación de Streaming.");
+                Console.WriteLine("9. Sistema de Biblioteca.");
+                Console.WriteLine("10. Vehículo Eléctrico.");
+                Console.WriteLine("\n0. Salir.");
+
                 Console.Write("\nSeleccione una opción: ");
 
                 if (!int.TryParse(Console.ReadLine(), out int opcion)) continue;
@@ -38,13 +48,80 @@ namespace Corte_1
                 switch (opcion)
                 {
                     case 1:
-                        Console.WriteLine(">>> EJERCICIO 1: MASCOTA VIRTUAL <<<");
+                        Console.WriteLine(">>> EJERCICIO 1: PERFIL GAMER <<<");
+                        Console.Write("Nickname: ");
+                        string nick = Console.ReadLine();
+                        Console.Write("Nivel (1-100): ");
+                        int lvl = int.Parse(Console.ReadLine());
+                        Console.Write("Suscripcion Premium? (1. Si / 2. No): ");
+                        bool esVip = Console.ReadLine() == "1";
+
+                        Perfil_Gamer perfil = new Perfil_Gamer(nick, lvl, esVip);
+                        perfil.MostrarBienvenida();
+                        break;
+                    case 2:
+                        Console.WriteLine(">>> EJERCICIO 2: CALCULADORA DE PROPINA SOLIDARIA <<<");
+                        Console.Write("Total de la cuenta: $ ");
+                        double total = double.Parse(Console.ReadLine());
+                        Console.Write("Porcentaje de propina (0-100): ");
+                        int porcentaje = int.Parse(Console.ReadLine());
+                        CalculadoraPropina calculadora = new CalculadoraPropina(total, porcentaje);
+                        calculadora.CalcularYMostrar();
+                        break;
+                    case 3:
+                        Console.WriteLine(">>> EJERCICIO 3: CONTROL DE AFORO <<<");
+
+                        Console.Write("¿Con cuantas personas inicia la discoteca?: ");
+                        int inicio = int.Parse(Console.ReadLine());
+                        ControlAforo sistemaAforo = new ControlAforo(inicio);
+
+                        bool seguirAforo = true;
+                        while (seguirAforo)
+                        {
+                            Console.Write("¿Cuantas personas quieren entrar? (O pulse 0 para volver al menu): ");
+                            int quieren = int.Parse(Console.ReadLine());
+
+                            if (quieren == 0)
+                            {
+                                seguirAforo = false;
+                            }
+                            else
+                            {
+                                sistemaAforo.ValidarIngreso(quieren);
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine(">>> EJERCICIO 4: GENERADOR DE CORREO <<<");
+                        Console.Write("Nombre: ");
+                        string nom = Console.ReadLine();
+
+                        Console.Write("Apellido: ");
+                        string ape = Console.ReadLine();
+
+                        Console.Write("Nombre de la empresa: ");
+                        string emp = Console.ReadLine();
+
+                        GeneradorCorreo correo = new GeneradorCorreo(nom, ape, emp);
+                        correo.MostrarCorreo();
+                        break;
+                    case 5:
+                        Console.WriteLine(">>> EJERCICIO 5: SIMULADOR DE SEMAFORO <<<");
+
+                        Console.Write("Ingrese el color actual (Verde, Amarillo, Rojo): ");
+                        string colorIngresado = Console.ReadLine();
+
+                        SemaforoInteligente miSemaforo = new SemaforoInteligente(colorIngresado);
+                        miSemaforo.MostrarAccion();
+                        break;
+                    case 6:
+                        Console.WriteLine(">>> EJERCICIO 6: MASCOTA VIRTUAL <<<");
                         Mascota mascota = new Mascota();
                         mascota.MostrarMascotas();
                         break;
 
-                    case 2:
-                        Console.WriteLine(">>> EJERCICIO 2: INVENTARIO <<<");
+                    case 7:
+                        Console.WriteLine(">>> EJERCICIO 7: INVENTARIO <<<");
                         Producto producto = new Producto("Smartphone Galaxy", 850000, 15);
                         Console.WriteLine($"Producto: {producto.Nombre}\nPrecio: ${producto.Precio_Producto}\nStock: {producto.Cantidad_Stock}");
                         Console.Write("\nCantidad a comprar: ");
@@ -52,8 +129,8 @@ namespace Corte_1
                         producto.VenderProducto();
                         break;
 
-                    case 3:
-                        Console.WriteLine(">>> EJERCICIO 3: STREAMING <<<");
+                    case 8:
+                        Console.WriteLine(">>> EJERCICIO 8: STREAMING <<<");
                         List<IReproductor> biblioteca = new List<IReproductor>
                         {
                             new Cancion("Bohemian Rhapsody", "Queen"),
@@ -70,23 +147,17 @@ namespace Corte_1
                             biblioteca[eleccion].Play();
                         break;
 
-                    case 4:
-                        Console.WriteLine(">>> EJERCICIO 4: BIBLIOTECA <<<");
+                    case 9:
+                        Console.WriteLine(">>> EJERCICIO 9: BIBLIOTECA <<<");
                         Libro libro = new Libro("El Principito");
                         Console.WriteLine($"Libro: {libro.titulo} | Estado: {(libro.disponible ? "Disponible" : "Prestado")}");
-
-                        Console.WriteLine("\n[Paso 1] Intentando préstamo...");
-                        libro.Prestar();
-
-                        Console.WriteLine("\n[Paso 2] Intentando préstamo de nuevo...");
                         libro.Prestar();
                         break;
 
-                    case 5:
-                        Console.WriteLine(">>> EJERCICIO 5: VEHÍCULO ELÉCTRICO <<<");
+                    case 10:
+                        Console.WriteLine(">>> EJERCICIO 10: VEHÍCULO ELÉCTRICO <<<");
                         VehiculoElectrico miCoche = new VehiculoElectrico("Porsche Taycan", 100);
                         Console.WriteLine($"Modelo: {miCoche.modelo} | Carga: {miCoche.bateria}%");
-
                         Console.Write("\nDistancia del viaje (km): ");
                         int km = int.Parse(Console.ReadLine());
                         miCoche.Viajar(km);
@@ -98,7 +169,7 @@ namespace Corte_1
                         break;
 
                     default:
-                        Console.WriteLine("Opción no válida.");
+                        Console.WriteLine("Opción no válida o aún no implementada.");
                         break;
                 }
 
