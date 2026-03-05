c60804d2ccbddcaad2851113a85bf9a388b94a77
Henry-COL <HenryQuinteroCO@gmail.com>
2026-03-05 12:41:55 -0500

    0.0.053

---
+++ TipoParcial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipoParcial.BibliotecaDigital.Interfaces;
+using TipoParcial.BibliotecaDigital.Modelos;
+using TipoParcial.BibliotecaDigital.Enums;
+
+using System;
+using System.Collections.Generic;
+using TipoParcial.BibliotecaDigital.Interfaces;
+
+namespace TipoParcial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
