f3c3a316ee8c9a28cb63ebb4c9998d751c448e18
Henry-COL <HenryQuinteroCO@gmail.com>
2026-03-12 17:31:32 -0500

    NewDate

---
+++ TipoParcial2/ConcesionarioVehiculos/Interfaces/IVendible.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoParcial2.ConcesionarioVehiculos.Interfaces
+{
+    public interface IVendible
+    {
+         decimal Precio { get; set; }
+         string Marca { get; set; }
+         string Modelo { get; set; }
+         int Año { get; set; }
+         Enumns.TipoCombustible TipoCombustible { get; set; }
+         Enumns.EstadoVehiculo EstadoVehiculo { get; set; }
+
+        public void MostrarInformacion();
+    }
+}
---
+++ TipoParcial2/ConcesionarioVehiculos/Modelos/Vehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoParcial2.ConcesionarioVehiculos.Modelos
+{
+    public class Vehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+
+        public
+    }
+}
