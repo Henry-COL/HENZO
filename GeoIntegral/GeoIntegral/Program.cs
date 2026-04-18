a3e528c8247587da07273e319804cdbef3bb6470
Henry-COL <HenryQuinteroCO@gmail.com>
2026-04-18 00:00:33 -0500

    aa

---
+++ GeoIntegral/GeoIntegral/Program.cs
@@ -0,0 +1,21 @@
+﻿using GeoIntegral.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeoIntegral
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoadScreen());
+        }
+    }
+}
