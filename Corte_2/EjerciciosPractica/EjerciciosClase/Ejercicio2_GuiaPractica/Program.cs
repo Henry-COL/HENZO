b5679c6b764c639e69752c8a73660687ccb5e631
Henry-COL <HenryQuinteroCO@gmail.com>
2026-04-20 00:51:15 -0500

    ccc

---
+++ Corte_2/EjerciciosPractica/EjerciciosClase/Ejercicio2_GuiaPractica/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ejercicio2_GuiaPractica
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+
+        }
+    }
+}
